7dbfa6311751c3157b9104b850b619e419fabc33
ricardoaugustob <ricardoaugustob@outlook.com>
2021-09-07 00:28:31 -0300

    Primeiro commit do c# console simples

---
+++ projeto1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o seu nome: ");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Olá "+ nome, " seja bem vindo" );
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            adulto(idade);
+
+
+            Console.ReadLine();
+        }
+
+        static void adulto(int idade)
+        {
+            if(idade >0 && idade < 18)
+            {
+                Console.WriteLine("Você não é adulto");
+
+            }
+            else if(idade>=18)
+            {
+                Console.WriteLine("Você é adulto!!");
+            }
+            else
+            {
+                Console.WriteLine("Idade fora do intervalo");
+            }
+        }
+    }
+}
